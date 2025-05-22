e602cf24fe5afd2dbbb72d726dcf38bfaf8efeb2
Wuttiphong2000 <wuttiphong.santhalunai@gmail.com>
2025-05-22 16:20:22 +0700

    cart pages/ placeOde pagesr

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,18 @@
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
 
 const Navbar = ({setShowLogin}) => {
 
   const [menu, setMenu] = useState("home");
 
+  const {getTotalCartAmount} = useContext(StoreContext);
+
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="logo" className="logo" />
+      <Link to="/"><img src={assets.logo} alt="logo" className="logo" /></Link>
       <ul className="navbar-menu">
         <li>
           <Link
@@ -51,8 +54,8 @@ const Navbar = ({setShowLogin}) => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="search" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="basket-icon" />
-          <div className="dot"></div>
+          <Link to="/cart"><img src={assets.basket_icon} alt="basket-icon" /></Link>
+          <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
         </div>
         <button onClick={()=>setShowLogin(true)}>sign in</button>
       </div>
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,12 +1,84 @@
-import React from 'react'
-import "./Cart.css"
+import { useContext } from "react";
+import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+  const { cartItem, food_list, removeFromCart, getTotalCartAmount } =
+    useContext(StoreContext);
 
-export default Cart
+  const navigate = useNavigate();
+
+  return (
+    <div className="cart">
+      <div className="cart-item">
+        <div className="cart-item-title">
+          <p>Item</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item) => {
+          if (cartItem[item._id] > 0) {
+            return (
+              <div>
+                <div className="cart-item-title cart-item-item">
+                  <img src={item.image} />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItem[item._id]}</p>
+                  <p>${item.price * cartItem[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                    X
+                  </p>
+                </div>
+                <hr />
+              </div>
+            );
+          }
+        })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>
+                ${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
+              </b>
+            </div>
+          </div>
+          <button onClick={() => navigate("/order")}>
+            PROCEED TO CHECKOUT
+          </button>
+        </div>
+        <div className="cart-promo-code">
+          <div>
+            <p>If you have a promo code, Enter it here</p>
+            <div className="cart-promo-code-input">
+              <input type="text" placeholder="promo code" />
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
